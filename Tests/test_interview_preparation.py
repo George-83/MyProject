a0c797017d217a97df66b49a371e192ae869f4ce
Georgii Bordukov <Georgii_Bordukov@epam.com>
2023-06-22 22:09:35 +0300

    Another commit from me

--- Tests/test_interview_preparation.py
+++ Tests/test_interview_preparation.py
@@ -181,3 +181,30 @@ x in y
 x not in y
 
 '''
+
+# list: a = ["apple", 23, "banana"]
+# dictionary: a = {"age": "27", "height": "180"}
+# tuple: a = ("apple", 27, "orange")
+# set: a = {"apple", 99, "banana"}
+
+
+'''
+Dictionary Methods
+
+clear()	Removes all the elements from the dictionary
+copy()	Returns a copy of the dictionary
+fromkeys()	Returns a dictionary with the specified keys and value
+get()	Returns the value of the specified key
+items()	Returns a list containing a tuple for each key value pair
+keys()	Returns a list containing the dictionary's keys
+pop()	Removes the element with the specified key
+popitem()	Removes the last inserted key-value pair
+setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+update()	Updates the dictionary with the specified key-value pairs
+values()	Returns a list of all the values in the dictionary
+'''
+
+
+def test_open_file():
+    file = open("D:/Home PC Part_2/Downloads/Music/reveille-inside-out.mp3")
+
