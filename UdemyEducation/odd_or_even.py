f6d627e03df038906bd59df57f31efc74d3c4fa0
George Bordukov <sair.jaingm@gmail.com>
2024-01-31 12:10:29 +0200

    Another commit from me

--- UdemyEducation/odd_or_even.py
+++ UdemyEducation/odd_or_even.py
@@ -1,4 +1,7 @@
 if __name__ == '__main__':
     # Odd or Even
-
-
+    number = int(input("Which number do you want to check? "))
+    if number % 2 == 0:
+        print("This is an even number.")
+    else:
+        print("This is an odd number.")
