7ba71ed604a76baacbde1169449663fcf5a284cf
George Bordukov <sair.jaingm@gmail.com>
2024-02-01 15:09:58 +0200

    Another commit from me

---
+++ UdemyEducation/pizza_order.py
@@ -0,0 +1,24 @@
+if __name__ == '__main__':
+    # Pizza Order Program
+    size = input("What size pizza do you want? S, M, or L ").upper()
+    add_pepperoni = input("Do you want pepperoni? Y or N ").upper()
+    extra_cheese = input("Do you want extra cheese? Y or N ").upper()
+    check = 0
+    if size == "S":
+        check = 15
+    elif size == "M":
+        check = 20
+    elif size == "L":
+        check = 25
+    else:
+        print("Please enter correct size - S, M, or L")
+    if add_pepperoni == "Y":
+        if size == "S":
+            check = 15 + 2
+        elif size == "M":
+            check = 20 + 3
+        else:
+            check = 25 + 3
+    if extra_cheese == "Y":
+        check += 1
+    print(f"Thank you for choosing Python Pizza Deliveries! \nYour final bill is: ${check}.")
