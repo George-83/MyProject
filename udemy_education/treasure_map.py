cd17afffad882d84c93a46a2f171f51a4592d770
George Bordukov <sair.jaingm@gmail.com>
2024-06-16 11:17:33 +0300

    Another commit from me

--- udemy_education/treasure_map.py
+++ udemy_education/treasure_map.py
@@ -14,4 +14,10 @@ if __name__ == '__main__':
     elif position_index == 'c':
         index = 2
     map[line][index] = 'X'
+    # Alternative variant:
+    # letter = position[0].lower()
+    # abc = ["a", "b", "c"]
+    # letter_index = abc.index(letter)
+    # number_index = int(position[1]) - 1
+    # map[number_index][letter_index] = "X"
     print(f"{line1}\n{line2}\n{line3}")
