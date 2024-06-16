469fe25b2e1c2e4c97e553d2608e3cbabc97c163
George Bordukov <sair.jaingm@gmail.com>
2024-06-16 14:39:23 +0300

    Another commit from me

--- udemy_education/treasure_map.py
+++ udemy_education/treasure_map.py
@@ -1,10 +1,12 @@
 if __name__ == '__main__':
+    line0 = ["A", "B️", "C️"]
     line1 = ["⬜️", "⬜️", "⬜️"]
     line2 = ["⬜️", "⬜️", "⬜️"]
     line3 = ["⬜️️", "⬜️️", "⬜️️"]
     map = [line1, line2, line3]
     print("Hiding your treasure! X marks the spot.")
-    position = input()  # Where do you want to put the treasure?
+    print(f"   {line0}\n1 {line1}\n2 {line2}\n3 {line3}")
+    position = input("Where do you want to put the treasure? Enter the coordinates. Example: C2. \nYour choice: ")
     position_index = position[0].lower()
     line = int(position[1]) - 1
     if position_index == 'a':
